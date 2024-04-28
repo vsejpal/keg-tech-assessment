f8521674688166597de96938493d96488b4b7d85
Vivek Sejpal <vsejpal@gmail.com>
2024-04-27 21:01:59 -0700

    commenting out feature that was causing page load warning

--- configs/postcss.config.js
+++ configs/postcss.config.js
@@ -1,10 +1,11 @@
 module.exports = {
     plugins: {
-        'postcss-preset-env': {
-            features: {
-                'color-adjust': false
-            }
-        },
+        /* removes the nasty error on the page upon initial load, that then needs to be X 'd out. */
+        // 'postcss-preset-env': {
+        //     features: {
+        //         'color-adjust': false
+        //     }
+        // },
         'cssnano': {
             preset: ['default', {
                 discardUnused: false // Prevents optimizations that might include color-adjust
